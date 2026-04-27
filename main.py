bb729222b7e57a2651927d407a1639ea2758c3c2
cfleung <u3560667@connect.hku.hk>
2026-04-28 00:36:13 +0800

    modify

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import random
 import requests
 import json
 import os
+from module.cache import CacheManager
 
 YF_PAGE_SIZE = 250
 YF_PAGE_SIZE = 250
@@ -48,46 +49,46 @@ TV_COLUMNS = [
 CACHE_DIR = "/content/drive/MyDrive/st/cache"
 
 
-class CacheManager:
-    cache: pd.DataFrame
-    cache_path: Path
+# class CacheManager:
+#     cache: pd.DataFrame
+#     cache_path: Path
 
-    def __init__(self, cache_dir: str = CACHE_DIR):
-        self.cache_dir = cache_dir
-        self.cache = pd.DataFrame()
-        self.cache_path = Path(os.path.join(
-            cache_dir, f"cache-{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.csv"))
-        os.makedirs(self.cache_dir, exist_ok=True)
+#     def __init__(self, cache_dir: str = CACHE_DIR):
+#         self.cache_dir = cache_dir
+#         self.cache = pd.DataFrame()
+#         self.cache_path = Path(os.path.join(
+#             cache_dir, f"cache-{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.csv"))
+#         os.makedirs(self.cache_dir, exist_ok=True)
 
-    def load(self):
-        cache_files = os.listdir(self.cache_dir)
-        cache_files.sort(reverse=True)
-        latest_cache_files = [os.path.join(
-            self.cache_dir, f) for f in cache_files if f.endswith('.csv')]
-        if latest_cache_files:
-            latest_cache_path = Path(latest_cache_files[0])
-            if latest_cache_path.exists():
-                self.cache_path = latest_cache_path
-                df = pd.read_csv(latest_cache_path, parse_dates=["timestamp"])
-                self.cache = df[df["timestamp"] > datetime.datetime.now() - datetime.timedelta(days=30)]
+#     def load(self):
+#         cache_files = os.listdir(self.cache_dir)
+#         cache_files.sort(reverse=True)
+#         latest_cache_files = [os.path.join(
+#             self.cache_dir, f) for f in cache_files if f.endswith('.csv')]
+#         if latest_cache_files:
+#             latest_cache_path = Path(latest_cache_files[0])
+#             if latest_cache_path.exists():
+#                 self.cache_path = latest_cache_path
+#                 df = pd.read_csv(latest_cache_path, parse_dates=["timestamp"])
+#                 self.cache = df[df["timestamp"] > datetime.datetime.now() - datetime.timedelta(days=30)]
 
-    def get_bsummary(self, ticker: str | None) -> str | None:
-        if self.cache.empty or ticker is None:
-            return None
-        row = self.cache[self.cache['ticker'] == ticker]
-        if not row.empty:
-            return row.iloc[0]['business_summary']
-        return None
+#     def get_bsummary(self, ticker: str | None) -> str | None:
+#         if self.cache.empty or ticker is None:
+#             return None
+#         row = self.cache[self.cache['ticker'] == ticker]
+#         if not row.empty:
+#             return row.iloc[0]['business_summary']
+#         return None
 
-    def append(self, ticker: str | None, bsummary: str | None):
-        if ticker is None or bsummary is None:
-            return
-        new_row = pd.DataFrame(
-            [{"ticker": ticker, "business_summary": bsummary, "timestamp": pd.to_datetime(datetime.datetime.now())}])
-        self.cache = pd.concat([self.cache, new_row], ignore_index=True)
+#     def append(self, ticker: str | None, bsummary: str | None):
+#         if ticker is None or bsummary is None:
+#             return
+#         new_row = pd.DataFrame(
+#             [{"ticker": ticker, "business_summary": bsummary, "timestamp": pd.to_datetime(datetime.datetime.now())}])
+#         self.cache = pd.concat([self.cache, new_row], ignore_index=True)
 
-    def save(self):
-        self.cache.to_csv(self.cache_path, index=False)
+#     def save(self):
+#         self.cache.to_csv(self.cache_path, index=False)
 
 
 def get_tickers(min_cap: int = 2_000_000_000,
@@ -194,7 +195,7 @@ def main():
     data_list = get_ticker_infos(tickers=tickers, cache_manager=cache_manager)
 
     df = pd.DataFrame(data_list)
-    out_dir = Path(f"/content/drive/MyDrive/st/output-{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}")
+    out_dir = Path(f"/content/drive/MyDrive/st/output-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}")
     out_dir.mkdir(exist_ok=True)
     output_csv = out_dir / "output.csv"
     df.to_csv(output_csv, index=False)
